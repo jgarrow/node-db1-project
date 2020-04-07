3c7ad9c11957b8297780eb9584263aa25aeb3e2d
Janessa Garrow <janessa.garrow@gmail.com>
2020-04-06 21:55:55 -0600

    Create CRUD methods

--- api/server.js
+++ api/server.js
@@ -1,9 +1,12 @@
 const express = require("express");
 
 const db = require("../data/dbConfig.js");
+const accountsRoutes = require("./routes/accountsRoute");
 
 const server = express();
+const baseUrl = "/api";
 
 server.use(express.json());
+server.use(`${baseUrl}`, accountsRoutes);
 
 module.exports = server;
